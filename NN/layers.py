eeecc04da89ebeb9a2c2f0d813d12b6e397fbb03
misha-posiba <misha@posiba.com>
2015-04-07 16:55:43 -0500

    initial commit of back propagation NN

---
+++ NN/layers.py
@@ -0,0 +1,167 @@
+__author__ = 'misha'
+
+import numpy as np
+
+
+class InputLayer(object):
+    """
+    Contains the basics: activations and size
+    Will serve as a base class
+    """
+
+    def __init__(self, size):
+
+        self.size = size
+        self.activations = None
+
+    def activate(self, inputs):
+        """
+        In this case activations are inputs
+        """
+
+        self.activations = np.array(inputs).reshape((-1, self.size))
+
+
+class HiddenLayer(InputLayer):
+    """
+    Hidden layer
+    """
+
+    def __init__(self, size, input_size, activate_f='log'):
+        """
+        Initialize with size and the size of the previous layer.
+        Activations of the previous layer are inputs for this layer
+        :param size: Number of neurons in this layer
+        :param input_size: Size of the previous layer
+        :param activate_f: Activation function, default: 'log', right now only logistic function is implemented
+        """
+        super(HiddenLayer, self).__init__(size)
+
+        # for being able to specify the activation function
+        self.activate_f = activate_f
+
+        self.input_size = input_size
+        self.inputs = None
+        self.input_plus_bias = None
+        self.deltas = None
+
+        # start with random weights
+        self.theta = 0.1 * np.random.rand(input_size + 1, size)
+        # these are derivatives of cost function with respect to weights, used for updating the weights during training
+        self.theta_update = np.zeros(self.theta.shape)
+
+        # number of training examples seen since last update of the weights
+        self.batch_counter = 0
+
+    @staticmethod
+    def _logistic(arr):
+        """
+        Logistic function
+        :param arr: input array of arguments
+        :return: array of results
+        """
+        arr = np.array(arr)
+        return 1. / (1. + np.exp(- arr))
+
+    def _activation_function(self, x):
+        """
+        This is for being able to switch between different activation functions
+        :param x: array of input arguments
+        :return: results
+        """
+        if self.activate_f == 'log':
+            return self._logistic(x)
+        else:
+            raise Exception("{} is unknown activation function".format(self.activate_f))
+
+    def _activation_derivative(self,):
+        """
+        Selection of activation function derivatives
+        :return: derivative values
+        """
+        if self.activations is None:
+            return None
+        elif self.activate_f == 'log':
+            return self.activations * (1. - self.activations)
+        else:
+            raise Exception("{} is unknown activation function".format(self.activate_f))
+
+    def _set_inputs(self, inputs):
+        """
+        Helper method to set layer inputs, make sure the shape is correct and to add biases
+        :param inputs: inputs to the layer (activations of the previous layer)
+        """
+        self.inputs = np.array(inputs).reshape((-1, self.input_size))
+
+        bias = np.ones((self.inputs.shape[0], 1))
+        self.input_plus_bias = np.concatenate((bias, self.inputs), axis=1)
+
+    def activate(self, inputs):
+        """
+        Propagates forward: calculates activations of this layer neurons from given inputs
+        :param inputs: layer inputs (activations of the previous layer)
+        :return:
+        """
+        self._set_inputs(inputs)
+
+        # sum up all inputs times the weights
+        z = np.dot(self.input_plus_bias, self.theta)
+        # calculate activations using activation function
+        self.activations = self._activation_function(z)
+
+    def set_deltas(self, pre_deltas):
+        """
+        Calculates deltas from propagated back deltas (or form targets)
+        :param pre_deltas: deltas x weights of the previous layer (or difference between outputs and targets)
+        :return:
+        """
+        if self.activations is None:
+            pass
+        else:
+            pre_d = np.array(pre_deltas).reshape((-1, self.size))
+            self.deltas = self._activation_derivative() * pre_d
+
+    def get_previous_pre_deltas(self,):
+        """
+        Calculate deltas x weights for the lower level
+        :return:
+        """
+        # remove the biases from result
+        return  np.dot(self.deltas, self.theta.T)[:, 1:]
+
+    def increment_updates(self,):
+        """
+        Add to the cost derivatives accumulator using deltas
+        """
+        self.theta_update += np.dot(self.input_plus_bias.T, self.deltas)
+        # increment the number of seen examples
+        self.batch_counter += self.deltas.shape[0]
+
+    def update_weights(self, alpha, la):
+        """
+        Update weights
+        :param alpha: learning rate
+        :param la: lambda - regularization coefficient
+        """
+        # update
+        self.theta -= float(alpha) * (self.theta_update / self.batch_counter + la * self.theta)
+        # reset examples counter and derivatives accumulator
+        self.theta_update = np.zeros(self.theta.shape)
+        self.batch_counter = 0
+
+
+class OutputLayer(HiddenLayer):
+    """
+    Same as Hidden layer, but with different deltas calculation
+    """
+
+    def set_deltas(self, targets):
+        """
+        Deltas are just the difference between outputs and targets
+        :param targets: Targets
+        :return:
+        """
+        if self.activations is None:
+            pass
+        else:
+            self.deltas = self.activations - np.array(targets).reshape((-1, self.size)).astype(np.float)
---
+++ NN/net.py
@@ -0,0 +1,208 @@
+__author__ = 'misha'
+
+import numpy as np
+
+from layers import InputLayer, HiddenLayer, OutputLayer
+
+
+class Net(object):
+    """
+    Class to assemble the network, train and use it
+    """
+
+    def __init__(self, layer_sizes, cost_f='log'):
+        """
+        Initialize network with the following parameters:
+        :param layer_sizes: list of layer sizes including input and output([2, 3, 4] - 2 neurons in the input layer,
+                            3 in the hidden layer, 4 in the output)
+        :param cost_f: optional, default is 'log' for log cost function
+        :return:
+        """
+
+        self.layer_sizes = layer_sizes
+        self.cost_f = cost_f
+
+        # list to keep track of cost values during training
+        self.costs = []
+
+        # outputs produced by the network, same as last layer activations
+        self.outputs = None
+
+        # instantiate layers
+        self.layers = []
+        for i, size in enumerate(self.layer_sizes):
+            if i == 0:
+                self.layers.append(InputLayer(size))
+            elif i == len(self.layer_sizes) - 1:
+                self.layers.append(OutputLayer(size, self.layer_sizes[i - 1]))
+            else:
+                self.layers.append(HiddenLayer(size, self.layer_sizes[i - 1]))
+
+    def _log_cost(self, outputs, targets, reg_coeff):
+        """
+        Log cost function
+        :param outputs: predicted outputs
+        :param targets: targets
+        :return: cost value
+        """
+        j = targets * np.log(outputs) + (1. - targets) * np.log(1. - outputs)
+        thetas_sq_sum = sum([(l.theta ** 2).sum() for l in self.layers[1:]])
+        e = (- j.sum() + reg_coeff * 0.5 * thetas_sq_sum) / float(j.shape[0])
+
+        return e
+
+    @staticmethod
+    def _split_into_batches(arr, batch_size):
+        """
+        Helper method to split arrays into batches (used for splitting training input and target arrays)
+        :param arr: given array of data
+        :param batch_size: Directly specify batch size by passing an integer.
+                        Alternatively can specify a fraction of the total length.
+                        If 0, negative number or None is passed - will create just one batch with the whole array.
+        :return: list of batches
+        """
+        arr_length = len(arr)
+        batches = list()
+
+        if batch_size is None or batch_size <= 0:
+            batches.append(arr)
+            return batches
+
+        # convert float size into integers
+        if type(batch_size) == float and 0 < batch_size < 1:
+            batch_size = max(np.round(batch_size * arr_length), 1)
+
+        # make sure batch_size is an integer (this will also deal with floats greater than 1)
+        batch_size = np.int(batch_size)
+
+        curr_index = 0
+        while curr_index < arr_length:
+            batches.append(arr[curr_index: curr_index + batch_size])
+            curr_index += batch_size
+
+        return batches
+
+    def forward(self, batch_inputs):
+        """
+        Perform forward pass through the network
+        :param batch_inputs: inputs to the network, can be single example or an array of examples
+        """
+        for i, l in enumerate(self.layers):
+            if i == 0:
+                l.activate(batch_inputs)
+            else:
+                l.activate(self.layers[i - 1].activations)
+
+        # for convenience
+        self.outputs = self.layers[-1].activations
+
+    def backward(self, batch_targets):
+        """
+        Propagate back the deltas (errors)
+        :param batch_targets: expected outputs of the network
+        """
+        # for the output layer pre-deltas are just targets, see OutputLayer class
+        pre_d = batch_targets
+        for l in self.layers[:0:-1]:
+            # calculate deltas for the current layer
+            l.set_deltas(pre_d)
+            # calculate theta updates using these deltas
+            l.increment_updates()
+            # calculate pre-deltas for the previous layer
+            pre_d = l.get_previous_pre_deltas()
+
+    def update_weights(self, update_rate, reg_coeff):
+        """
+        Update weight for all layers
+        :param update_rate: learning rate
+        """
+        for l in self.layers[:0:-1]:
+            l.update_weights(update_rate, reg_coeff)
+
+    def cost(self, targets, reg_coeff):
+        """
+        Calculate cost value for the current batch.
+        :param targets: current batch expected targets
+        :return:
+        """
+        if self.cost_f == 'log':
+            return self._log_cost(self.outputs, targets, reg_coeff)
+        else:
+            raise Exception("{} - unknown cost function".format(self.cost_f))
+
+    def fit(self, X, Y,
+            batch_size=None,
+            update_at='batch',
+            reg_coeff=0.,
+            update_rate=1.,
+            max_iter=1000,
+            min_cost=0.001):
+        """
+        Training of the network
+        :param X: training inputs (list/array of examples)
+        :param Y: training targets (list/array of examples)
+        :param batch_size: size of batches to split training set into
+        :param update_at: Specify when to update weights:
+                        'batch' - after each batch,
+                        'epoch' - after going through the whole dataset
+                        If any other value, the weights will never be updated.
+        :param reg_coeff: regularization coefficient
+        :param update_rate: learning rate
+        :param max_iter: Maximum number of epochs (going once through the whole dataset)
+        :param min_cost: Stop training if cost below this value. The training will also stop if cost variation during
+                        last 10 epochs was less than 0.01 * min_cost.
+        """
+
+        if len(Y) != len(X):
+            raise Exception("Lengths of X and Y should be the same")
+
+        # split into batches
+        batches = zip(self._split_into_batches(X, batch_size),
+                      self._split_into_batches(Y, batch_size))
+
+        counter = 0
+        stopping_condition = False
+
+        while not stopping_condition and counter < max_iter:
+            # cost accumulator
+            j = 0
+            for batch_inputs, batch_targets in batches:
+                # forward
+                self.forward(batch_inputs)
+
+                # calculate cost and add to the accumulator
+                j += self.cost(batch_targets, reg_coeff)
+
+                # backward
+                self.backward(batch_targets)
+
+                # maybe update weights
+                if update_at == 'batch':
+                    self.update_weights(update_rate, reg_coeff)
+
+            # maybe update weights
+            if update_at == 'epoch':
+                self.update_weights(update_rate, reg_coeff)
+
+            # keep track of the cost values
+            self.costs.append(j / float(len(batches)))
+
+            counter += 1
+
+            # update stopping condition
+            stopping_condition = (self.costs[-1] < min_cost)
+            if counter > 10:
+                stopping_condition = stopping_condition or \
+                                     (max(self.costs[-1:-10:-1]) - min(self.costs[-1:-10:-1])) < min_cost / 100.
+
+    def predict(self, X):
+        """
+        Perform forward pass only and return the results
+        :param X: list/array of inputs
+        :return: list/array of resulting outputs
+        """
+
+        self.forward(X)
+        return self.outputs
+
+
